78b62a0c8408bf075728e1a94e6c9688eaeaea7a
aqsakhannx3 <akhannx3@outlook.com>
2021-12-23 06:21:53 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ function scrollFunction() {
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
   ) {
+
+    
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
